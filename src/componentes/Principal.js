a5010d0df456b5296dcba413bfabae249eb82c22
Xavi-e <xesgawork@gmail.com>
2021-06-22 13:04:27 +0200

    bugfix componente principal

--- src/componentes/Principal.js
+++ src/componentes/Principal.js
@@ -1,18 +1,6 @@
 
 export const Principal = () => {
     return (
-        <body>
-            <header class="cabecera espaciado bloque-superior">
-                <h1>Lista de la compra</h1>
-                <nav>
-                    <ul class="navegacion">
-                        <li><a href="principal">Principal</a></li>
-                        <li class="actual"><a href="lista">Lista</a></li>
-                        <li><a href="acerca-de">Acerca de</a></li>
-                    </ul>
-                </nav>
-            </header>
-            <main class="home"><h2>Bienvenido a la lista de la compra</h2></main>
-        </body>
+        <main class="home"><h2>Bienvenido a la lista de la compra</h2></main>
     );
 };
