9ae5d1ccc6705c949b0d56bd546228c4af6f153a
Xavi-e <xesgawork@gmail.com>
2021-06-22 13:24:28 +0200

    componente acercaDe suubido.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@
 import { Principal } from "./componentes/Principal";
 import { Cabecera } from "./componentes/Cabecera";
 import { Formulario } from "./componentes/Formulario";
+import { Lista } from "./componentes/Lista";
+import { AcercaDe } from "./componentes/AcercaDe";
+
 function App() {
   return (
     <>
@@ -12,55 +15,10 @@ function App() {
       <Principal />
       {/*======================PRINCIPAL====================*/}
       {/*====================ACERCA DE======================*/}
-      <main className="acerca-de">
-        <h2>Acerca de Lista de la compra</h2>
-        <p>
-          Lorem ipsu dolor sit amet consectetur adipisicing elit. Autem officia
-          odio reiciendis vel architecto molestiae, saepe temporibus beatae
-          perferendis iste quod excepturi, sit molestias assumenda? Officia
-          dolorum similique placeat enim.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse
-          eveniet, doloremque odit nobis accusamus distinctio autem architecto
-          provident veritatis, odio, maiores deserunt repellat iure explicabo
-          laboriosam pariatur exercitationem illum aspernatur?
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quasi,
-          ullam inventore non, perspiciatis necessitatibus, quos error optio et
-          dolor temporibus eaque voluptatum! Amet fugit, ad aspernatur minima
-          sunt adipisci.
-        </p>
-      </main>
+      <AcercaDe />
       {/*====================ACERCA DE======================*/}
       {/*====================LISTA======================*/}
-      <section className="info espaciado bloque-superior">
-        <i className="icono fas fa-plus-circle"></i>
-        <p className="n-articulos">2/3 comprados</p>
-      </section>
-      <main className="principal espaciado">
-        <ul className="articulos">
-          <li className="articulo">
-            <input type="checkbox" className="marcar" />
-            <span className="nombre">Pan</span>
-            <span className="precio">0.75€</span>
-            <i className="borrar fas fa-times"></i>
-          </li>
-          <li className="articulo">
-            <input type="checkbox" className="marcar" />
-            <span className="nombre">Azúcar</span>
-            <i className="borrar fas fa-times"></i>
-          </li>
-          <li className="articulo">
-            <input type="checkbox" className="marcar" />
-            <span className="nombre">Leche</span>
-            <span className="precio">1.20€</span>
-            <i className="borrar fas fa-times"></i>
-          </li>
-        </ul>
-        <span className="precio-total">1.95€</span>
-      </main>
+      <Lista />
       {/*====================LISTA======================*/}
       {/*====================FORMULARIO======================*/}
       <Formulario />
